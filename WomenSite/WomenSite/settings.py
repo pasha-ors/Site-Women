1c7557f910fa544fd8261e985ada9ff84809b896
Pavlo <pas.haors79@gmail.com>
2024-07-30 23:09:43 +0200

    set up the router

--- WomenSite/WomenSite/settings.py
+++ WomenSite/WomenSite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'women.WomenConfig',
+    'women.apps.WomenConfig',
 ]
 
 MIDDLEWARE = [
--- WomenSite/women/views.py
+++ WomenSite/women/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Web Page women")
+
+
+def categories(request):
+    return HttpResponse("<h1>Web Page Categories</h1>")
