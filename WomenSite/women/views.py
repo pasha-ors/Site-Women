fe90c1651cd1bd42630f12871483cf27305de2d3
Pavlo <pas.haors79@gmail.com>
2024-07-30 23:41:41 +0200

    Add Converter and Dynamic urls

--- WomenSite/women/views.py
+++ WomenSite/women/views.py
@@ -7,5 +7,11 @@ def index(request):
     return HttpResponse("Web Page women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Web Page Categories</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Web Page Categories</h1><p>id: {cat_id}</p>")
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Web Page Categories</h1><p>slug: {cat_slug}</p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Year archive:</h1><p>year: {year}</p>")
