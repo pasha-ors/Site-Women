50413a4ee2a2e9ebf2e844f7ff1d277d34dd3781
Pavlo <pas.haors79@gmail.com>
2024-07-31 19:53:39 +0200

    redirection

--- WomenSite/women/views.py
+++ WomenSite/women/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 
@@ -14,8 +16,9 @@ def categories_by_slug(request, cat_slug):
     return HttpResponse(f"<h1>Web Page Categories</h1><p>slug: {cat_slug}</p>")
 
 def archive(request, year):
-    if  year > 2024:
-        raise Http404()
+    if year > 2024:
+        uri = reverse('cats', args=('music', ))
+        return redirect(uri)
     return HttpResponse(f"<h1>Year archive:</h1><p>year: {year}</p>")
 
 def page_not_found(request, exception):
